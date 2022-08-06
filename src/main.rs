ad601fd8107913da4468f69c10d340f014847b73
Kyle Rader <kylewrader@gmail.com>
2022-08-05 23:26:13 -0700

    Impl Display on concrete structs for what we care about

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,55 @@
-fn main() -> Result<(), reqwest::Error> {
-    const USER: &str = "AzureAD";
-    const REPO: &str = "microsoft-authentication-cli";
+use std::fmt::Display;
 
-    let url = format!("https://api.github.com/repos/{USER}/{REPO}/releases");
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
+struct Asset {
+    name: String,
+    size: usize,
+    download_count: usize,
+}
+
+impl Display for Asset {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}\n\t\tbytes: {}\n\t\tdownloads: {}\n",
+            self.name, self.size, self.download_count
+        )
+    }
+}
+
+#[derive(Deserialize)]
+struct Release {
+    name: Option<String>,
+    tag_name: Option<String>,
+    // created_at: String, // we can do better
+    assets: Vec<Asset>,
+}
+
+impl Display for Release {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} @ {}\n",
+            self.name.clone().unwrap_or("<unnamed>".into()),
+            self.tag_name.clone().unwrap_or("<untagged>".into())
+        )?;
+
+        for a in self.assets.clone() {
+            write!(f, "\t{}", a)?;
+        }
+        write!(f, "")
+    }
+}
+
+fn main() -> Result<(), reqwest::Error> {
+    // const USER: &str = "AzureAD";
+    // const REPO: &str = "microsoft-authentication-cli";
+    const USER: &str = "rust-lang";
+    const REPO: &str = "rust";
+
+    let url = format!("https://api.github.com/repos/{USER}/{REPO}/releases?per_page=100");
 
     let client = reqwest::blocking::Client::builder()
         .user_agent("github-stats-cli")
@@ -10,12 +57,10 @@ fn main() -> Result<(), reqwest::Error> {
 
     let response = client.get(url).send()?;
 
-    let data: serde_json::Value = response.json()?;
+    let data: Vec<Release> = response.json()?;
 
-    if let Some(data) = data.as_array() {
-        for r in data {
-            println!("{}", r["name"]);
-        }
+    for r in data {
+        println!("{r}");
     }
     Ok(())
 }
